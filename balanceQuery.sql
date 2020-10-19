31f1a14b734f049cdf1627d967dfceaca2cef276
ahmedelsakk <65472244+ahmedelsakk@users.noreply.github.com>
2020-10-19 12:06:40 +0200

    Add files via upload

--- balanceQuery.sql
+++ balanceQuery.sql
@@ -1,3 +1,4 @@
+
  --create customer balance update
  SELECT t1.*, t2.BillingModel,t2.[Consolidation Cycle],t3.[Warranty Days]
   INTO Customer_balance_update 
@@ -24,65 +25,31 @@ where  t.[Invoice Type]='accounting trans' or t.[Invoice Type]='Accountig Trans 
 select c.[Customer Number] into balance_review  from [Customer Balance] c
 ---------------------------------------------
 
-  ----------------------------------
+ --update customer balance "consolidation cycle"
  update Customer_balance_update
  set [Consolidation Cycle]='Every 1 Month'
  where [Consolidation Cycle] is null
  --------------------------------------------
+ 
+ --update customer balance "BillingModel"
  update Customer_balance_update
  set BillingModel='In Advance'
  where BillingModel is null
  ----------------------------------------------
- select * from Customer_balance_update
- ------------------------------
--- DECLARE @date DATETIME = '8/30/2020';  
--- SELECT DATEPART(QUARTER, @date)
- 
---SELECT DATEADD (dd, -1, DATEADD(qq, DATEDIFF(qq, 0, GETDATE()) +1, 0))
-----to get last day in aquarter
---SELECT DATEADD (dd, -1, DATEADD(qq, DATEDIFF(qq, 0, '2020-9-2') +1, 0)) from Customer_balance_update
--- ---------------------------------------
--- --To get the last day of the current year:
-
---SELECT DATEADD (dd, -1, DATEADD(yy, DATEDIFF(yy, 0,[Invoice / Receipt Date] ) +1, 0)) from Customer_balance_update
---------------------------------------------------------
----- to get last day of amonth
---   select EOMONTH([Invoice / Receipt Date]), EOMONTH([Invoice / Receipt Date],1) from Customer_balance_update
-
---      select EOMONTH([Invoice / Receipt Date],3-MONTH([Invoice / Receipt Date])%3) from Customer_balance_update
---	    select EOMONTH('2020-08-12',3-MONTH('2020-08-12')%3) 
-		---------------------------------------------------------------
-		--add new column 
-	ALTER TABLE Customer_balance_update
+ --add end_of_period column
+ALTER TABLE Customer_balance_update
 ADD End_of_period date;
------------------------------------------------------------------------------
----insert into end of period
---update t
---set End_of_period=(case
-----when t.BillingModel='In advance' then ([Invoice / Receipt Date])
---when t.[Consolidation Cycle]='Every 1 Month' then EOMONTH([Invoice / Receipt Date])
---when t.[Consolidation Cycle]='Every 3 Months' then EOMONTH([Invoice / Receipt Date],3-MONTH([Invoice / Receipt Date])%3)
---when t.[Consolidation Cycle]='Every 6 Months' then EOMONTH([Invoice / Receipt Date],6-MONTH([Invoice / Receipt Date])%6)
---when t.[Consolidation Cycle]='Every 12 Months' then EOMONTH([Invoice / Receipt Date],12-MONTH([Invoice / Receipt Date])%12)
---end)
---from Customer_balance_update t
-select End_of_period  from Customer_balance_update
+
 --------------------------------------
 update t 
 set End_of_period=(case
---when t.BillingModel='In advance' then ([Invoice / Receipt Date])
-when t.[Consolidation Cycle]='Every 1 Month' then ([Invoice / Receipt Date])
+
+when t.[Consolidation Cycle]='Every 1 Month' then EOMONTH([Invoice / Receipt Date])
 when t.[Consolidation Cycle]='Every 3 Months' then DATEADD (dd, -1, DATEADD(qq, DATEDIFF(qq, 0, [Invoice / Receipt Date]) +1, 0))
 when t.[Consolidation Cycle]='Every 6 Months' then IIF(MONTH([Invoice / Receipt Date])<=6,concat(year([Invoice / Receipt Date]),'-','06-30') ,DATEADD(YEAR, DATEDIFF(YEAR, 0, [Invoice / Receipt Date]) + 1, -1) ) 
 when t.[Consolidation Cycle]='Every 12 Months' then DATEADD(YEAR, DATEDIFF(YEAR, 0, [Invoice / Receipt Date]) + 1, -1) 
-
 end)
 from Customer_balance_update t
-
-select t.[Invoice / Receipt Date],t.End_of_period,t.DueDate2
- from Customer_balance_update t where t.[Consolidation Cycle]='Every 6 Months'
--- IIF(DATEDIFF(DAY,[Invoice / Receipt Date],@endDatePart1)>=0,@endDatePart1,@endDatePart2)
-----------------------------------------------------------------------------------
 
 -------------------------------------------------------------------------------
 -- add due date and insert
@@ -95,21 +62,18 @@ when t.BillingModel='In arrears' then DATEADD(day,[Warranty Days],t.End_of_perio
 end)
 from Customer_balance_update t
 ---------------------------------------------------
-select sum([Functional Balance]) from Customer_balance_update
---------------------------------------------------------------------
+
 --add due
 ALTER TABLE Customer_balance_update ADD Due decimal(20,4);
 ----------------------------------------------------
 update t
 set due =(case
 when t.Type='payment' then 0
-when t.duedate2<='2020-10-11' then [Functional Balance]
+when t.duedate2<=GETDATE() then [Functional Balance]
 else 0
 end)
 from Customer_balance_update t
 
-select sum(Due) as due,sum(NotDue) as notDue from Customer_balance_update
-select sum(due) due,sum(NotDue) notdue,sum([functional balance without onaccount]) unapplied,sum(OnAccount2) onaccount from Customer_balance_update
 ------------------------------------------------------------------
 ----add not due
 ALTER TABLE Customer_balance_update ADD NotDue decimal(20,4);
@@ -117,7 +81,7 @@ ALTER TABLE Customer_balance_update ADD NotDue decimal(20,4);
 update t
 set NotDue =(case
 when t.Type='payment' then 0
-when t.duedate2>'2020-10-11' then [Functional Balance]
+when t.duedate2>GETDATE() then [Functional Balance]
 else 0
 end)
 from Customer_balance_update t
@@ -144,9 +108,7 @@ else 0
 end)
 from Customer_balance_update t
 -----------------------------------------------
-select Number,sum(due) due,sum(notdue) notdue,sum([functional balance without onaccount]) unapplied, sum(OnAccount2) onAccount from Customer_balance_update 
-group by Number
- select sum(OnAccount2) from Customer_balance_update
+
 ----------------------------------------------------------
 ----add due to balance review
 
@@ -204,6 +166,7 @@ update balance_review
 set customer_name= tempName.account_name from tempName right outer join balance_review
 on tempName.[Account Number]=balance_review.[Customer Number]
 ----------------------------------------------------
+--- add parent_name
 ALTER TABLE balance_review
 ADD parent_name nvarchar(255);
 -------------------------------------
@@ -213,6 +176,7 @@ update balance_review
 set parent_name= tempParent.parent from tempParent right outer join balance_review
 on tempParent.[Account Number]=balance_review.[Customer Number]
 ---------------------------------------------------------------------
+--add customer type
 ALTER TABLE balance_review
 ADD [Customer Type]  nvarchar(255);
 --------------------------------------------------------
@@ -222,6 +186,7 @@ update balance_review
 set [Customer Type]=customer_type  from tempType right outer join balance_review
 on tempType.[Account Number]=balance_review.[Customer Number]
 --------------------------------------------------------------
+--add Customer Category
 ALTER TABLE balance_review
 ADD [Customer Category]  nvarchar(255);
 ----------------------------------------------------
@@ -231,6 +196,7 @@ update balance_review
 set [Customer Category]=customer_category from tempCategory right outer join balance_review
 on tempCategory.[Account Number]=balance_review.[Customer Number]
 ------------------------------------------------------------------------
+--add Market Segment
 ALTER TABLE balance_review
 ADD [Market Segment]  nvarchar(255);
 -------------------------------------------------------
@@ -240,6 +206,7 @@ update balance_review
 set [Market Segment] =market from tempMarket right outer join balance_review
 on tempMarket.[Account Number]=balance_review.[Customer Number]
 ---------------------------------------------------------------------------------------
+--add Business Line
 ALTER TABLE balance_review
 ADD [Business Line]  nvarchar(255);
 -----------------------------------------------
@@ -267,12 +234,10 @@ update balance_review
 set [Customer Admin Status] =AdminStatus from tempAdmin right outer join balance_review
 on tempAdmin.[Account Number]=balance_review.[Customer Number]
 ----------------------------------------------------------------------
-select * from ch153
 
----------------------------
 ALTER TABLE balance_review
 ADD [max date]  date;
-
+-----------------------------------------------
 ;with tempDate as(select [Customer Number] customerNumber,MAX([Receipt Date]) maxDate from  CH153 group by [Customer Number] 
 )
 update balance_review
@@ -281,7 +246,7 @@ on tempDate.customerNumber=balance_review.[Customer Number]
 --------------------------------------------------------------
 ALTER TABLE balance_review
 ADD [Last Receipt Amount]  decimal;
-
+------------------------------------------------------
 ;with tempAmount as(select [Customer Number] customerNumber,[Receipt Date] mDate,sum([Receipt Amount Functional]) amount from  CH153 
 group by [Receipt Date],[Customer Number])
 update balance_review
@@ -291,6 +256,7 @@ on (tempAmount.customerNumber=balance_review.[Customer Number] and tempAmount.mD
 
 ALTER TABLE balance_review
 ADD [Oldest Unsettled Invoice or Debit Note]  date;
+------------------------------------------------------
 
 ;with tempOldDate as(select Number,min([Invoice / Receipt Date]) OldDate
  from  TE_Data_Customer_Balance_Detai t
@@ -317,44 +283,5 @@ update balance_review
  set onAccount=0
  where onAccount is null
  --------------------------------------------
-select * from balance_review 
- 
-
- select sum(due) from balance_review
-
- select * from Customer_balance_update t
- where t.[Invoice / Receipt Number]='E-1368773'
 
 
-
-
- select EOMONTH('2020-3-31',3-MONTH('2020-3-31')%3) from balance_review
-
-
- select 
-    case when month(GETDATE()) <= 6 
-        then 
-            datepart(dayofyear , getdate())
-        else
-            datediff(day, dateadd(month, 6, dateadd(year, datediff(year, 0, getdate()), 0)), getdate()) + 1
-    end
-
-
-
-	SELECT DATEADD(MONTH, 6, DATEADD(YEAR, DATEDIFF(YEAR, 0, '2010-10-3'), 0)) from Customer_balance_update
-
-	SELECT DATEPART(DAYOFYEAR,GETDATE())
-
-	DEclare  @endDatePart1 date ,@endDatePart2 date 
-	set @endDatePart1 ='2020-06-30'
-	set @endDatePart2 ='2020-12-31'
-	select IIF(DATEDIFF(DAY,[Invoice / Receipt Date],@endDatePart1)>=0,@endDatePart1,@endDatePart2)
-	
-
-	DEclare @year date
-	set @year=YEAR('2020-02-10')
-	select @year+'06-30'
-
-	select sum(t.Due) from Customer_balance_update t where t.Number='S49917'
-
-	select t.[Invoice / Receipt Date],t.[Warranty Days],t.DueDate2 from Customer_balance_update t where t.BillingModel='in advance'
